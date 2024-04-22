6025701cabde46c54e938469b50ab8fac817319a
S1mp3r <99444306+S1mp3r@users.noreply.github.com>
2024-04-22 20:33:15 -0300

    Update q1_RafaelCarneiro.py

--- AV2_RafaelCarneiro/q1_RafaelCarneiro.py
+++ AV2_RafaelCarneiro/q1_RafaelCarneiro.py
@@ -15,9 +15,9 @@ def fund_Transfer(user_Fund, user_name):
     print("Provide Bank Deposit Details: " + "\nUsername: " + user_name + " // Credits:%.2f" % user_Fund)
     print("----------------------------------------------")
 
-    str_Choice = input("Confirm or Cancel the Transaction type ('Confirm' or 'Cancel'): ")
+    str_Choice = lambda: input("Confirm or Cancel the Transaction type ('Confirm' or 'Cancel'): ")
 
-    return choices_from_user(str_Choice)
+    return choices_from_user(str_Choice())
 
 def credit(user_Fund, user_name):
     choices_from_user = lambda x : "Transaction Completed" if x == "Confirm" else "Close Transaction"
@@ -27,25 +27,28 @@ def credit(user_Fund, user_name):
     print("Provide Credit Account Details: " + "\nUsername: " + user_name + " // Credits: %.2f" % user_Fund)
     print("----------------------------------------------")
 
-    str_Choice = input("Confirm or Cancel the Transaction type ('Confirm' or 'Cancel'): ")
+    str_Choice = lambda: input("Confirm or Cancel the Transaction type ('Confirm' or 'Cancel'): ")
 
-    return choices_from_user(str_Choice)
+    return choices_from_user(str_Choice())
 
-create_transaction = lambda x : cash_Transaction(user_Fund) if x == "Cash" else (
-    fund_Transfer(user_Fund, user_name) if x == "Fund Transfer" else (
-    credit(user_Fund, user_name) if x == "Credit" else "Incorrect Value"))
+create_transaction = lambda x : cash_Transaction(user_Fund()) if x == "Cash" else (
+    fund_Transfer(user_Fund(), datas[0]) if x == "Fund Transfer" else (
+    credit(user_Fund(), datas[0]) if x == "Credit" else "Incorrect Value"))
 
 print("Creating Transaction...!\n")
 
 user_name = input("Please write down below your Username: ")
 user_pssw = input("Please write down below your Password: ")
 
-user_name_Logado = "UserLog"
-user_pssw_Logado = "PassLog"
+datas[0] = user_name()
+datas[1] = user_pssw()
+
+user_name_Logado = lambda: "UserLog"
+user_pssw_Logado = lambda: "PassLog"
 
 user_Fund = round(random.uniform(1, 5000), 2)
 
-transaction_type = input("Enter transaction type (Cash, Fund Transfer, Credit): ")
+transaction_type = lambda: input("Enter transaction type (Cash, Fund Transfer, Credit): ")
 
-result = create_transaction(transaction_type)
-print(result)
+result = lambda: create_transaction(transaction_type())
+print(result())
